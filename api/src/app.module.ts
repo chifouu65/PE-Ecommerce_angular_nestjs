69284a7014ce507e9a26a9bdefd09f666e73ebc7
Noah <89213023+chifouu65@users.noreply.github.com>
2023-08-31 20:17:25 +0200

    .

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { ProductsModule } from './modules/products/products.module';
-import { OrdersService } from './modules/orders/orders.service';
-import { OrdersController } from './modules/orders/orders.controller';
 import { OrdersModule } from './modules/orders/orders.module';
 import { UsersModule } from './modules/users/users.module';
-import { AuthService } from './modules/auth/auth.service';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, UsersModule, AuthModule],
-  controllers: [AppController, OrdersController, ],
-  providers: [AppService, OrdersService, AuthService, ],
+  imports: [
+    ProductsModule, 
+    OrdersModule, 
+    UsersModule, 
+    AuthModule
+  ],
+  controllers: [
+    AppController, 
+  ],
+  providers: [
+    AppService, 
+  
+  ],
 })
 export class AppModule {}
--- api/src/modules/orders/orders.module.ts
+++ api/src/modules/orders/orders.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
 
-@Module({})
+@Module({
+    controllers: [OrdersController],
+    providers: [OrdersService]
+})
 export class OrdersModule {}
